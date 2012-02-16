2a3c9f4d6189a364e6ab99f5c595591db13c6d28
John David Back <johndavidback@gmail.com>
2012-02-15 21:05:23 -0500

    Adding some sweet gravatar support booyaaa

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var crypto = require('crypto');
+
 
 var port = 1342;
 
@@ -28,11 +30,12 @@ var everyone = nowjs.initialize(httpServer);
 var chat_group = nowjs.getGroup('chat-group');
 
 var user_names = Array();
+var user_hashes = Array();
 
 // var everyone = nowjs.initialize(server);
 
 everyone.now.distributeMessage = function(msg) {
-	everyone.now.receiveMessage(this.now.name, this.user.clientId, msg);
+	everyone.now.receiveMessage(this.now.name, user_hashes[this.user.clientId], this.user.clientId, msg);
 	console.log("Incoming message [" + this.user.clientId + "]: " + msg);
 };
 
@@ -44,7 +47,7 @@ everyone.now.directMessage = function(id, msg) {
 
 	nowjs.getClient(id, function() {
 		console.log('Found user ' + this.now.name);
-		this.now.receiveMessage('Direct From: ' + from_user, user_id, message);
+		this.now.receiveMessage('Direct From: ' + from_user, user_hashes[user_id], user_id, message);
 	});
 };
 
@@ -58,9 +61,15 @@ nowjs.on('connect', function() {
 	
 	var client_id = this.user.clientId;
 	var name = this.now.name;
+	var email = this.now.email;
 	var the_user = this.now;	
 
 	user_names[client_id] = name;
+
+	// Let's get the hash so we can use it with gravatar!
+	var md5hash = crypto.createHash('md5');
+	md5hash.update(email);
+	user_hashes[client_id] = md5hash.digest('hex');
 
 	chat_group.getUsers(function(users) {
 		for(var i = 0; i < users.length; i++) {
@@ -71,7 +80,7 @@ nowjs.on('connect', function() {
 	chat_group.addUser(this.user.clientId);
 	chat_group.now.addUserToList(this.now.name, this.user.clientId);
 
-	chat_group.now.receiveMessage('Server', 0, name + ' has joined the chat. Say hello!');
+	chat_group.now.receiveMessage('Server', 0, 0, name + ' has joined the chat. Say hello!');
 
 	console.log('User ' + name + ' with id ' + client_id + ' is now online');
 
@@ -82,6 +91,8 @@ nowjs.on('disconnect', function() {
 	var name = this.now.name;
 
 	console.log('User ' + name + ' with id ' + client_id + ' is now offline');
+
+	chat_group.now.receiveMessage('Server', 0, 0, name + ' has left us. For shame.');
 
 	chat_group.removeUser(client_id);
 	everyone.now.removeUserFromList(client_id);
